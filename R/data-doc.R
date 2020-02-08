c9ea8b0896189dcbfded7295c3a48438cd721297
Petr Bouchal <pbouchal@gmail.com>
2020-02-08 14:48:47 +0100

    export remaining Prague codes, closes #20

---
+++ R/data-doc.R
@@ -0,0 +1,114 @@
+#' Prague RUIAN code
+#'
+#' Prague RUIAN code
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague RUIAN code
+#' }
+#' @export
+"prg_kod"
+
+#' Prague ICO code
+#'
+#' Prague ICO code
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague ICO code
+#' }
+#' @export
+"prg_ico"
+
+#' Prague NUTS2 code
+#'
+#' Prague NUTS2 code
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague NUTS2 code
+#' }
+#' @export
+"prg_nuts2"
+
+#' Prague NUTS3 code
+#'
+#' Prague NUTS3 code, level of kraj. Comes from CZSO registry 108 - NUTS3-2004.
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague NUTS3 code
+#' }
+#' @export
+"prg_nuts3"
+
+#' Prague LAU1 code
+#'
+#' Prague LAU1 code - level of okres.
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague LAU1 code. Comes from CZSO registry 109 - OKRES_LAU.
+#' }
+#' @export
+"prg_lau1"
+
+#' Prague 'okres' code
+#'
+#' Prague 'okres' code. Comes from CZSO registry 101 - OKRES_NUTS.
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague okres code
+#' }
+#' @export
+"prg_okres"
+
+#' Prague 'kraj' code
+#'
+#' Prague 'kraj' code. Comes from CZSO registry 100, KRAJ_NUTS.
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague kraj code
+#' }
+#' @export
+"prg_kraj"
+
+#' Prague 'okres LAU' code
+#'
+#' Prague 'okres LAU' code. Comes from CZSO register 101 (OKRES_NUTS) - yes these are
+#' confusing but that is how it is.
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague okres (NUTS) code.
+#' }
+#' @export
+"prg_okres_nuts"
+
+#' Prague code in OECD database of Functional Urban Areas
+#'
+#' Prague code in OECD Functional Urban Area database.
+#'
+#' See https://www.oecd.org/cfe/regional-policy/functionalurbanareasbycountry.htmz
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague code in OECD Functional Urban Area database.
+#' }
+#' @export
+"prg_fua_oecd"
+
+#' Prague code in OECD CITIES database of data on Functional Urban Areas
+#'
+#' Prague code in OECD database on Metropolitan Areas.
+#'
+#' See https://stats.oecd.org/Index.aspx?DataSetCode=CITIES
+#'
+#' @format character vector of length 1
+#' \describe{
+#'   Prague code in OECD Functional Urban Area database.
+#' }
+#' @export
+"prg_metro_oecd"
---
+++ data-raw/data.R
@@ -0,0 +1,29 @@
+prg_kod <- '554782'
+usethis::use_data(prg_kod, overwrite = T)
+
+prg_ico <- '00064581'
+usethis::use_data(prg_ico, overwrite = T)
+
+prg_nuts2 <- 'CZO1'
+usethis::use_data(prg_nuts2, overwrite = T)
+
+prg_nuts3 <- 'CZ010'
+usethis::use_data(prg_nuts3, overwrite = T)
+
+prg_lau1 <- 'CZ0100'
+usethis::use_data(prg_lau1, overwrite = T)
+
+prg_okres <- '3100'
+usethis::use_data(prg_okres, overwrite = T)
+
+prg_kraj <- '3018'
+usethis::use_data(prg_kraj, overwrite = T)
+
+prg_okres_nuts <- '40924'
+usethis::use_data(prg_okres_nuts, overwrite = T)
+
+prg_fua_oecd <- 'CZ001'
+usethis::use_data(prg_fua_oecd, overwrite = T)
+
+prg_metro_oecd <- 'CZ001L2'
+usethis::use_data(prg_metro_oecd, overwrite = T)
